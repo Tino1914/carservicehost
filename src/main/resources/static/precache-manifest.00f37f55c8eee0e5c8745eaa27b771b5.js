34606407f0c25827e9fa886cd18773f81aa49828
Konstantin Hadjiyankov <k.hadjiyankov@student.fontys.nl>
2020-10-09 01:36:06 +0200

    react

---
+++ src/main/resources/static/precache-manifest.00f37f55c8eee0e5c8745eaa27b771b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26dad4afca529e03150292848a4c6f22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a1aeb377be85dec3d7",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e730e4518c86c8297bd6",
+    "url": "/static/css/main.25784feb.chunk.css"
+  },
+  {
+    "revision": "d7a1aeb377be85dec3d7",
+    "url": "/static/js/2.3b821af8.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/static/js/2.3b821af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e730e4518c86c8297bd6",
+    "url": "/static/js/main.6fa1be01.chunk.js"
+  },
+  {
+    "revision": "d4b12de24580ed6ee910",
+    "url": "/static/js/runtime-main.9cf836ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
